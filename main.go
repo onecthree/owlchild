311ad325bd208b2ddc048f778e051c460cb9bc18
onecthree <onecthree@gmail.com>
2022-11-11 17:27:13 +0700

    update fix

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import(
 	// . "github.com/onecthree/owlchild/facetype"
 	"github.com/onecthree/owlchild/database/odm/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 	// "github.com/kr/pretty"	
 
-	"fmt"
+	// "fmt"
 )
 
 type Podcast struct {
@@ -18,20 +18,19 @@ type Podcast struct {
 	Tags   []string           `bson:"tags,omitempty"`
 }
 
+type UsersInterface struct {
+	ID     		primitive.ObjectID 	`bson:"_id,omitempty"`
+	Username 	string             	`bson:"username,omitempty"`
+	PhoneNumber string				`bson:"phone_number,omitempty`
+}
+
 func main() {
 	
 	uri := "mongodb://simon:SimonProject135$!@localhost:27017";
-
-	Users := model.SetCollection(uri, "DbTest", "CollectionTest");
-
-	var UsersSet Podcast;
-
-	Users.FindOne( bson.M {
-		"title" : "Ini Judul",
-	}).Get();
-
-	Users.Assign(&UsersSet);
-
-	fmt.Println(UsersSet.Title);
+	models := model.SetCollection(uri, "DbTest", "CollectionTest");
+	models.Create(UsersInterface {
+		Username:		"Sultan Ilham",
+		PhoneNumber:	"081210981006",
+	});
 
 }
